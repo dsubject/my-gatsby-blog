b514a84e66df624ffccfa3a461290f507c70a58f
dani <daniellesubject@gmail.com>
2022-04-22 18:47:43 -0400

    updating config and adding green software post

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,12 +7,18 @@ module.exports = {
         link: '/'
       }
     ],
-    description: "Named after a column she used to write at the University of Guelph's student newspaper, Changing the Subject is a blog written by Danielle Subject. Her posts explore (but are not limited to) tech, the environment, and mental health.",
+    description:
+      "Named after a column she used to write at the University of Guelph's student newspaper, Changing the Subject is a blog written by Danielle Subject. Her posts explore (but are not limited to) tech, the environment, and mental health.",
     siteUrl: 'https://www.changingthesubject.ca',
     twitterUsername: '@danisubject',
     image: '/homeBanner.png',
     author: 'Danielle Subject',
-    keywords: ['green software', 'green tech', 'software developer', 'beekeeping']
+    keywords: [
+      'green software',
+      'green tech',
+      'software developer',
+      'beekeeping'
+    ]
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +26,8 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `markdown-pages`,
-        path: `${__dirname}/src/markdown-pages`,
-      },
+        path: `${__dirname}/src/markdown-pages`
+      }
     },
     `gatsby-plugin-sharp`,
     {
@@ -38,17 +44,18 @@ module.exports = {
               withWebp: true,
               withAvif: true,
               quality: 50
-            },
-          },
-        ],
-      },
+            }
+          }
+        ]
+      }
     },
     `gatsby-plugin-gatsby-cloud`,
     `gatsby-transformer-sharp`,
+    {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: "UA-87156604-2",
+        trackingId: 'UA-87156604-2',
         // Defines where to place the tracking script - `true` in the head and `false` in the body
         head: false,
         // Setting this parameter is optional
@@ -56,24 +63,24 @@ module.exports = {
         // Setting this parameter is also optional
         respectDNT: true,
         // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        exclude: ['/preview/**', '/do-not-track/me/too/'],
         // Delays sending pageview hits on route update (in milliseconds)
         pageTransitionDelay: 0,
         // Enables Google Optimize using your container Id
-        optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        optimizeId: 'YOUR_GOOGLE_OPTIMIZE_TRACKING_ID',
         // Enables Google Optimize Experiment ID
-        experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+        experimentId: 'YOUR_GOOGLE_EXPERIMENT_ID',
         // Set Variation ID. 0 for original 1,2,3....
-        variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+        variationId: 'YOUR_GOOGLE_OPTIMIZE_VARIATION_ID',
         // Defers execution of google analytics script after page load
         defer: false,
         // Any additional optional fields
         sampleRate: 5,
         siteSpeedSampleRate: 10,
-        cookieDomain: "example.com",
+        cookieDomain: 'example.com',
         // defaults to false
-        enableWebVitalsTracking: true,
-      },
-    },
-  ],
-};
+        enableWebVitalsTracking: true
+      }
+    }
+  ]
+}
